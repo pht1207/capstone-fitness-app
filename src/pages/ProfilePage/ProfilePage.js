29d338d35e188e804c52ab4677f57c0fe32d2fe8
pht1207 <parkerthroneberry@gmail.com>
2024-02-16 09:24:33 -0600

    Added comments to NutritionPage.js showing what methods are used to fetch food items

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -3,7 +3,7 @@ import './ProfilePage.css'
 function ProfilePage() {
 
   //Your code to make the site functional goes in this empty space. The 'return()' below is what renders on the page (the html)
-
+  
 
 
 
