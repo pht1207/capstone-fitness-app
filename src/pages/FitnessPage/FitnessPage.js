de5b57c553cc6fd2275da97b104e304f01fa89c7
pht1207 <parkerthroneberry@gmail.com>
2024-02-16 09:22:49 -0600

    Added comments to FitnessPage.js that contain the methods needed for fetching exercises/workouts as well as creating them

--- src/pages/FitnessPage/FitnessPage.js
+++ src/pages/FitnessPage/FitnessPage.js
@@ -3,7 +3,10 @@ import './FitnessPage.css'
 function FitnessPage() {
 
   //Your code to make the site functional goes in this empty space. The 'return()' below is what renders on the page (the html)
-
+  //Inserted by parker: https://capstone.parkert.dev/backend/getExercises
+  //https://capstone.parkert.dev/backend/createExercises
+  //https://capstone.parkert.dev/backend/getWorkouts
+  //https://capstone.parkert.dev/backend/createWorkouts
 
 
 
