725251ce73a30fd832d4ca49612f84c64fa4cae5
pht1207 <parkerthroneberry@gmail.com>
2024-02-19 10:19:57 -0600

    Added comments and onSubmit methods for the forms that will be used in LoginPage.js and Register.js

--- src/pages/LoginRegistrationPage/RegisterPage.js
+++ src/pages/LoginRegistrationPage/RegisterPage.js
@@ -3,9 +3,15 @@ import './RegisterPage.css'
 function RegisterPage() {
 
   //Your code to make the site functional goes in this empty space. The 'return()' below is what renders on the page (the html)
+
   //Inserted by parker: https://capstone.parkert.dev/backend/login
-
-
+  
+  /* Added by Parker to be used later. Leave this code for now.
+  function onSubmit(e){
+    e.preventDefaults();
+    console.log("form submitted");
+  }
+  */
 
 
   return (
