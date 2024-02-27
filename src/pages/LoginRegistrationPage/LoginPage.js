201a31e925c1c5cfbc3822ecd7ac99636ae71186
JayySlimmy <jayyslimthekidd@gmail.com>
2024-02-27 12:23:48 -0600

    Added code to login.js

--- src/pages/LoginRegistrationPage/LoginPage.js
+++ src/pages/LoginRegistrationPage/LoginPage.js
@@ -4,21 +4,31 @@ import RegisterPage from './RegisterPage';
 function LoginPage() {
 
   //Your code to make the site functional goes in this empty space. The 'return()' below is what renders on the page (the html)
-
-  //Inserted by parker: https://capstone.parkert.dev/backend/login
-
-  /* Added by Parker to be used later. Leave this code for now.
-  function onSubmit(e){
-    e.preventDefaults();
-    console.log("form submitted");
-  }
-  */
+  
 
 
 
   return (
     <div className="LoginPage">
         <h1>This is the login page</h1>
+        <form id="login">
+            <fieldset>
+                <legend>Login</legend>
+                    <label>Email: </label>
+                    <input type="email" name="email" placeholder="Email Address" required/>
+                    {/*<!--<span id="emailError" style="color:red"></span>-->*/}<br/>
+                    <label>Password: </label>
+                    <input type="password" name="password" placeholder="Password" required/>
+                    {/*<!--<span id="passwordError" style="color:red"></span>-->*/}<br/>
+                {/*<!--How to save the user information, so they can login?-->
+                <!--Forgot Password-->*/}
+                {/*<!--The system shall verify if an email exists, so that it doesn't tell people that an email and password is wrong, and they have more than one email, so they don't know which one.-->*/}
+                <a href="HomePage.js"><button type="submit">Login</button></a><br/>
+                <fieldset>
+                    <legend>Not registered to login?: </legend><br/><a href="RegisterPage.js"><button type="button" formnovalidate>Register</button></a>
+                </fieldset>
+            </fieldset>
+        </form>
     </div>
   );
 }
