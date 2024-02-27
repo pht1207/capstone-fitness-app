fc942e6da2857f0708fbf7025d00c542f61904ee
FabianG5 <vic9fabian@gmail.com>
2024-02-27 11:53:07 -0600

    Profile Page & Edit Profile Page
    This is the profile page html and js code to create the edit profile page and the profile page

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -1,16 +1,91 @@
 import './ProfilePage.css'
+import './EditProfilePage.css'
+import { useState } from 'react';
 
 function ProfilePage() {
-
-  //Your code to make the site functional goes in this empty space. The 'return()' below is what renders on the page (the html)
+  const [editProfileClicked, setEditProfileClicked] = useState(true)
   
+function editClicked(){
+  //make code to toggle editprofileclicked to false or true
+  setEditProfileClicked(!editProfileClicked);
+}
 
+//do ternary operator to toggle html
 
 
   return (
-    <div className="ProfilePage">
-        <h1>This is the profile page</h1>
-    </div>
+    editProfileClicked ? (
+    <div class="ProfilePage">
+      <h1>PROFILE</h1>
+
+        <div class="UserI">
+          <div><p>First Name:</p><p>placeholder</p></div>
+          <div><p>Last Name:</p><p>placeholder</p></div>
+        </div>
+
+        <div class="UserI">
+          <div><p>Username:</p><p>placeholder</p></div>
+          <div><p>Email:</p><p>placeholder</p></div>
+        </div>
+
+        <div class="UserI">
+          <div><p>Password:</p><p>placeholder</p></div>
+        </div>
+
+        <div class="UserI">
+          <div><p>Height:</p><p>placeholder</p></div>
+          <div><p>Weight:</p><p>placeholder</p></div>
+        </div>
+
+        <div class="UserI">
+          <div><p>Goal:</p><p>placeholder</p></div>
+        </div>
+
+        <div class="UserI">
+          <div><p>notifications:<label class="switch" ><input type="checkbox"/><span class="slider"></span></label></p></div>
+        </div>
+
+        <div class="UserI">
+          <div><button class="button1" onClick={editClicked}>EDIT</button></div>
+        </div>
+      </div>
+      ):(
+        <div class="ProfilePage">
+        <h1>UPDATE PROFILE</h1>
+  
+          <div class="UserI">
+            <div><p>First Name:</p><input type='text' placeholder='Name'/></div>
+            <div><p>Last Name:</p><input type='text' placeholder='Name'/></div>
+          </div>
+  
+          <div class="UserI">
+            <div><p>Username:</p><input type='text' placeholder='Name'/></div>
+            <div><p>Email:</p><input type='text' placeholder='Name'/></div>
+          </div>
+  
+          <div class="UserI">
+            <div><p>Password:</p><input type='password' placeholder='Name'/></div>
+          </div>
+  
+          <div class="UserI">
+            <div><p>Height:</p><input type='text' placeholder='Name'/></div>
+            <div><p>Weight:</p><input type='text' placeholder='Name'/></div>
+          </div>
+  
+          <div class="UserI">
+            <div><p>Goal:</p><select>
+                                <option value="option1">Option 1</option>
+                                <option value="option2">Option 2</option>
+                                <option value="option3">Option 3</option>
+                              </select>
+            </div>
+          </div>
+  
+          <div class="UserI">
+            <div><button class="button2" onClick={editClicked}>Save</button><button class="button3" onClick={editClicked}>Cancel</button></div>
+          </div>
+        </div>
+      )
   );
 }
 
