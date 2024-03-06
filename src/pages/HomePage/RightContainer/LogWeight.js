8b930cf0b380a466f09a8a494a066d5199dda41e
pht1207 <parkerthroneberry@gmail.com>
2024-03-06 09:41:48 -0600

    Added CSS to LogWeight.js component making it neatly fit in its box

--- src/pages/HomePage/RightContainer/LogWeight.js
+++ src/pages/HomePage/RightContainer/LogWeight.js
@@ -3,15 +3,18 @@ import axios from 'axios';
 
 function LogWeight() {
   
+  function WeightLogSubmit(event){
+    event.preventDefault();
+  }
 
 
   return (
     <div className="LogWeight">
         <h3>Log Weight</h3>
-        <form>
-          <input className='WeightInput'/>
-          <input type='date'/>
-          <input type='submit'/>
+        <form onSubmit={WeightLogSubmit} className='LogWeightForm'>
+          <label>Weight: <input className='WeightInput' type='number'/></label>
+          <label>Date Logged: <input type='date'></input></label>
+          <button type='submit'>Submit</button>
         </form>
 
   </div>
