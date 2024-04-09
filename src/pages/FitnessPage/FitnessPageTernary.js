5c5fe65c31d7d410855fa42ba0f0e645530af220
pht1207 <parkerthroneberry@gmail.com>
2024-04-09 16:59:49 -0500

    Added comments to fitnesspageternary.js describing what each function does

--- src/pages/FitnessPage/FitnessPageTernary.js
+++ src/pages/FitnessPage/FitnessPageTernary.js
@@ -2,6 +2,7 @@ import React,{useEffect, useState} from 'react';
 import AdvancedFitness from './AdvancedMode/AdvancedFitness';
 import BeginnerHome from './BeginnerMode/BeginnerHome';
 function FitnessPageTernary() {
+    //If there isn't a beginnerboolean set, set ti to true
     useEffect(()=>{
       if(localStorage.getItem("beginnerBoolean") === null){
         localStorage.setItem("beginnerBoolean", true);
@@ -9,13 +10,13 @@ function FitnessPageTernary() {
     })
 
     const [isBeginner, setIsBeginner] = useState(JSON.parse(localStorage.getItem("beginnerBoolean")));
-    
+    //If beginner mode is set, set it in localstorage
     useEffect(()=>{
       localStorage.setItem("beginnerBoolean", isBeginner)
     },[isBeginner])
 
   return (
-    <>
+    <>  {/* Shows beginnermode or advanced mode depending ont he value of isbeginner */}
         {isBeginner ? <BeginnerHome setIsBeginner={setIsBeginner}/> : <AdvancedFitness setIsBeginner={setIsBeginner}/>}
     </>
   );
