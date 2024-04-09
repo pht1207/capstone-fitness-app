70ceff88d44dcdb24da68d3e402162fd786d137a
pht1207 <parkerthroneberry@gmail.com>
2024-04-09 16:50:09 -0500

    Added comments to weightgraph.js describing what each function does and removed some redundant code

--- src/pages/HomePage/WeightGraph.js
+++ src/pages/HomePage/WeightGraph.js
@@ -6,9 +6,9 @@ import {HttpPopupContext} from '../../components/HttpPopupContext';
 function WeightGraph(props) {
 
   const [axiosData, setAxiosData] = useState();
-  const [dataObject, setDataObject] = useState()
   const {setResponse} = useContext(HttpPopupContext);
 
+  //This useEffect gets the user's weight log object and stores it to be used in the linechart component
   useEffect(()=>{
     async function getUserWeightLog(){
       try{
@@ -18,7 +18,7 @@ function WeightGraph(props) {
           }
         });
         setResponse(axiosResponse) //used in httpopup.js
-        setAxiosData(axiosResponse.data.results)
+        setAxiosData(axiosResponse.data.results) //This is the object that stores all the weight entries
       }
       catch(error){
         console.error("error: ", error.response)
@@ -29,22 +29,18 @@ function WeightGraph(props) {
   },[props.weightLogged])
   
 
-  useEffect(()=>{
-    setDataObject(axiosData)
-  },[axiosData])
-
   return (
     <div className="WeightGraph">
       <h4>Weight Graph</h4>
       <ResponsiveContainer width="100%" height="100%">
-        <LineChart data={dataObject}
+        <LineChart data={axiosData} 
           margin={{right: 20}}>
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="dateTimeChanged" />
-          <YAxis />
+          <XAxis dataKey="dateTimeChanged" /> {/* Sets the x axis to the date */}
+          <YAxis dataKey="weight"/> {/* Y axis is set to the weight */}
           <Tooltip />
           <Legend />
-          <Line type="monotone" dataKey="weight" stroke="#8884d8" />
+          <Line type="monotone" dataKey="weight" stroke="#8884d8" /> {/* The line for the graph is set to each entries' weight and time */}
         </LineChart>
       </ResponsiveContainer>
 
