4060fc66c88c1183d6cec8f979ff50dc9ea31017
pht1207 <parkerthroneberry@gmail.com>
2024-04-09 16:41:32 -0500

    Changed the way some functions were written in the homepage files so they could be more readable

--- src/pages/HomePage/WeightGraph.js
+++ src/pages/HomePage/WeightGraph.js
@@ -3,50 +3,36 @@ import { LineChart, XAxis, YAxis, Line, CartesianGrid, Tooltip, Legend, Responsi
 import axios from 'axios';
 import {HttpPopupContext} from '../../components/HttpPopupContext';
 
-
-
-
-
 function WeightGraph(props) {
 
   const [axiosData, setAxiosData] = useState();
   const [dataObject, setDataObject] = useState()
   const {setResponse} = useContext(HttpPopupContext);
 
-
   useEffect(()=>{
-    //Runs when the component is rendered
+    async function getUserWeightLog(){
+      try{
+        const axiosResponse = await axios.get("https://capstone.parkert.dev/backend/getUserWeightLog", {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("jwt")
+          }
+        });
+        setResponse(axiosResponse) //used in httpopup.js
+        setAxiosData(axiosResponse.data.results)
+      }
+      catch(error){
+        console.error("error: ", error.response)
+        setResponse(error.response)
+      }
+    }
     getUserWeightLog();
   },[props.weightLogged])
   
-  async function getUserWeightLog(){
-    try{
-      const axiosResponse = await axios.get("https://capstone.parkert.dev/backend/getUserWeightLog", {
-        headers: {
-          Authorization: "Bearer " + localStorage.getItem("jwt")
-        }
-      });
-      console.log(axiosResponse)
-      setResponse(axiosResponse) //used in httpopup.js
-      setAxiosData(axiosResponse.data.results)
-    }
-    catch(error){
-      console.error("error: ", error.response)
-      setResponse(error.response)
-    }
-  }
-
-
 
   useEffect(()=>{
-    //Called when axiosData is changed, sorts through it and puts it in a data object usable by <LineChart>
-    axiosDataFormatter();
+    setDataObject(axiosData)
   },[axiosData])
 
-  function axiosDataFormatter(){
-    setDataObject(axiosData)
-  }
-  
   return (
     <div className="WeightGraph">
       <h4>Weight Graph</h4>
